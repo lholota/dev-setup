4f5cfbc921cf42dd4c2c5b974d68593dd5188292
Lukas Holota <me@lholota.com>
2024-01-15 22:18:58 +0100

    GH release and SOPS

--- init.sh
+++ init.sh
@@ -6,9 +6,6 @@ if [[ $UID != 0 ]]; then
     exit 1
 fi
 
-echo "Updating apt..."
-apt update
-
 echo "Installing pip..."
 apt install -y python3-pip
 
@@ -16,4 +13,3 @@ echo "Installing Ansible via pip..."
 python3 -m pip install ansible
 
 echo "Done :)"
-echo "Please don't forget to re-open shell to allow the change of PATH variable to take effect (otherwise ansible will not be in PATH)"
