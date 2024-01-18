34347623c064673c7562fd024bc62a3f14878198
Lukas Holota <me@lholota.com>
2024-01-18 14:29:39 +0100

    Core script

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+case $1 in
+    init)
+        if [[ $UID != 0 ]]; then
+            echo "This command must be run with sudo permissions."
+            exit 1
+        fi
+
+        echo "Installing pip..."
+        apt install -y python3-pip
+
+        echo "Installing Ansible via pip..."
+        python3 -m pip install ansible
+
+        echo "Installing dependencies from ansible galaxy..."
+        ansible-galaxy install -r requirements.yaml
+      ;;
+    install)
+        echo "Installing dependencies from ansible galaxy..."
+        ansible-galaxy install -r requirements.yaml
+      ;;
+    apply)
+        ansible-playbook -i inventory.yaml setup.yaml ${@:2}
+      ;;
+    *) 
+      echo "'$1' is not a known command. Please use one of the commands below:"
+      echo "\t init \t- ansible and dependencies to prepare for playbook execution"
+      echo "\t install \t- installs only dependencies from ansible galaxy"
+      echo "\t apply \t- executes the core playbook, you can add --tags <tag1,tag2> to execute only specific parts"
+      ;;
+esac
