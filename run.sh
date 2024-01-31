d07479e4c9f61e993d971c47344d67250306eae8
Lukas Holota <me@lholota.com>
2024-01-31 14:40:25 +0100

    Nautilus back

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+writeCommands() {
+  echo "  init        - ansible and dependencies to prepare for playbook execution"
+  echo "  install     - installs only dependencies from ansible galaxy"
+  echo "  apply       - executes the core playbook, you can add --tags <tag1,tag2> to execute only specific parts"
+  echo "  apply:work  - executes the core playbook for work machines"
+  echo "  edit-vars   - editing vault protected variables"
+}
+
 case $1 in
     init)
         if [[ $UID != 0 ]]; then
@@ -22,14 +30,17 @@ case $1 in
     apply)
         ansible-playbook -i inventory.yaml setup.yaml --vault-password-file vault.key ${@:2}
       ;;
+    apply:work)
+        ansible-playbook -i inventory.yaml setup.yaml --extra-vars "workstation_type=work" --vault-password-file vault.key ${@:2}
+      ;;
     edit-vars)
         ansible-vault edit ./host_vars/localhost.yaml
       ;;
+    help)
+      writeCommands
+      ;;
     *) 
       echo "'$1' is not a known command. Please use one of the commands below:"
-      echo "\t init \t- ansible and dependencies to prepare for playbook execution"
-      echo "\t install \t- installs only dependencies from ansible galaxy"
-      echo "\t apply \t- executes the core playbook, you can add --tags <tag1,tag2> to execute only specific parts"
-      echo "\t edit-vats \t- editing vault protected variables"
+      writeCommands
       ;;
 esac
