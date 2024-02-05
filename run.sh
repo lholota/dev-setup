f7f5c47ef38a063c50deceac9f974dc423859ab5
Lukas Holota <me@lholota.com>
2024-02-05 12:54:54 +0100

    Checkpoint

--- run.sh
+++ run.sh
@@ -7,21 +7,18 @@ writeCommands() {
   echo "  edit-vars   - editing vault protected variables"
 }
 
+if [[ $UID == 0 ]]; then
+    echo "Please do not run this script as sudo."
+    exit 1
+fi
+
 case $1 in
     init)
-        if [[ $UID != 0 ]]; then
-            echo "This command must be run with sudo permissions."
-            exit 1
-        fi
-
         echo "Installing pip..."
-        apt install -y python3-pip
+        sudo apt install -y python3-pip
 
         echo "Installing Ansible via pip..."
-        python3 -m pip install ansible
-
-        echo "Installing dependencies from ansible galaxy..."
-        ansible-galaxy install -r requirements.yaml
+        sudo python3 -m pip install ansible
       ;;
     install)
         echo "Installing dependencies from ansible galaxy..."
